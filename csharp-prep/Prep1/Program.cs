78310bc85f54798e11feabefa3a1bf753db3f227
Yukari Hall <yukarihall@gmail.com>
2024-02-28 11:55:57 -0700

    first assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //this is the first week assignment
         Console.WriteLine("Hello Prep1 World!");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!!!!");
+        Console.WriteLine("This is my third change, Hello World!");
     }
 }
