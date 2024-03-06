eb544bd44548d3286c5855fb9c5e46c4247d3ae7
Yukari Hall <yukarihall@gmail.com>
2024-03-05 20:48:04 -0700

    a little change

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Registered Nurse";
+        job1._jobTitle = "Health Care Aide";
         job1._company = "Alberta Health Services";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._startYear = 2015;
+        job1._endYear = 2018;
 
         Job job2 = new Job();
         job2._jobTitle = "Licensed Practical Nurse";
         job2._company = "Alberta Health Services";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._startYear = 2018;
+        job2._endYear = 2020;
 
         Job job3 = new Job();
-        job3._jobTitle = "Health Care Aide";
+        job3._jobTitle = "Registered Nurse";
         job3._company = "Alberta Health Services";
         job3._startYear = 2020;
         job3._endYear = 2023;
