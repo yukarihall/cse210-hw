456b26331ae4d2615e65fb01d1d17e31c2598638
Yukari Hall <yukarihall@gmail.com>
2024-04-09 15:29:36 -0600

    Final1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -8,15 +8,15 @@ class Program
         // Create videos
         List<Video> videos = new List<Video>();
 
-        Video video1 = new Video("Title 1", "Author 1", 300);
-        video1.AddComment("User1", "Comment 1");
-        video1.AddComment("User2", "Comment 2");
-        video1.AddComment("User3", "Comment 3");
+        Video video1 = new Video("Fishing", "Takashi Kido", 300);
+        video1.AddComment("User1 Taro", "Your video is fun!");
+        video1.AddComment("User2 Ray", "You are doing great!");
+        video1.AddComment("User3 Jasmin", "Great!!");
         videos.Add(video1);
 
-        Video video2 = new Video("Title 2", "Author 2", 450);
-        video2.AddComment("User4", "Comment 1");
-        video2.AddComment("User5", "Comment 2");
+        Video video2 = new Video("Japanese 101", "Pompoko", 450);
+        video2.AddComment("User4 Kaitlyn", "a little hard!");
+        video2.AddComment("User5 John", "I know some words!");
         videos.Add(video2);
 
         // Iterate through videos
